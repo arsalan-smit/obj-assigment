fad01150d32f66aee16ac20a57b8ff702f1252db
arsalan-smit <arsalangg05@gmail.com>
2024-10-14 18:16:07 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// alert('hello');
+// var user=prompt("enter name")
+// alert(`hello ${user}`);
+// var age=+prompt("enter your age");
+// alert(`i am ${age} years old`);
+// legal
+// myVariable
+// user_age
+// totalAmount1
+// _privateVar
+// firstName
+// illegal
+// 1stVariable
+// my-variable
+// total amount
+// function
+// @myVar
+// var a = 10;
+// var b = 20;
+// var sum = (a+b);
+// var sub = (a-b);
+// var Multi = (a*b);
+// var divv = (a/b);
+// document.write(`sum = ${sum} subtraction = ${sub} Multiplication = ${Multi} division = ${divv}`);
